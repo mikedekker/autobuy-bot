32fb27f5837122959538f5f8ef0aa948b495ac2a
Mike Dekker <mike@dekker.me>
2021-02-22 14:56:28 +0100

    :bug: Fix caching

--- PS5AutoBuyer.py
+++ PS5AutoBuyer.py
@@ -944,13 +944,15 @@ def main():
             # generate headers
             # user_agent = random.choice(user_agents)
             headers = {
+                'pragma': 'no-cache',
+                'cache-control': 'no-cache',
+                'dnt': '1',
                 "User-Agent": user_agent,
                 "Accept-Encoding": "gzip, deflate",
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                 'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-                'sec-fetch-site': 'same-origin',
+                'sec-fetch-site': 'none',
                 'sec-fetch-mode': 'navigate',
-                'sec-fetch-user': '?1',
                 'sec-fetch-dest': 'document',
                 'referer': referer,
                 "Connection": "close", "Upgrade-Insecure-Requests": "1"
